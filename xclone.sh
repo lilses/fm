5b178ddae1479bdb2cdd4eb44ed9498fa112cb54
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-12 18:49:51 +0000

    refactor: edit code

---
+++ xclone.sh
@@ -0,0 +1,62 @@
+#git config -f .gitmodules submodule.<path>.branch <branch>
+# rust
+#git submodule add https://github.com/lilses/lib_stripe  &&
+#git config -f .gitmodules submodule.lib_stripe.branch master &&
+#git submodule add https://github.com/lilses/macros_make_app &&
+#git config -f .gitmodules submodule.macros_make_app.branch master &&
+#git submodule add https://github.com/lilses/lib_transferwise &&
+#git config -f .gitmodules submodule.lib_transferwise.branch master &&
+#git submodule add https://github.com/lilses/my_state &&
+#git config -f .gitmodules submodule.my_state.branch master &&
+#git submodule add https://github.com/lilses/macros_make_error &&
+#git config -f .gitmodules submodule.macros_make_error.branch master &&
+#git submodule add https://github.com/lilses/macros_make_scope &&
+#git config -f .gitmodules submodule.macros_make_scope.branch master &&
+#git submodule add https://github.com/lilses/macros_make_model &&
+#git config -f .gitmodules submodule.macros_make_model.branch master &&
+#git submodule add https://github.com/lilses/lib_wallet &&
+#git config -f .gitmodules submodule.lib_wallet.branch master &&
+#git submodule add https://github.com/lilses/lib_notification &&
+#git config -f .gitmodules submodule.lib_notification.branch master &&
+#git submodule add https://github.com/lilses/lib_fcm &&
+#git config -f .gitmodules submodule.lib_fcm.branch master &&
+#git submodule add https://github.com/lilses/lib_chat &&
+#git config -f .gitmodules submodule.lib_chat.branch master &&
+#git submodule add https://github.com/lilses/lib_auth_id &&
+#git config -f .gitmodules submodule.lib_auth_id.branch master &&
+#git submodule add https://github.com/lilses/macros_make_api_doc &&
+#git config -f .gitmodules submodule.macros_make_api_doc.branch master &&
+#git submodule add https://github.com/lilses/lib_typesense &&
+#git config -f .gitmodules submodule.lib_typesense.branch master &&
+#git submodule add https://github.com/lilses/lib_product &&
+#git config -f .gitmodules submodule.lib_product.branch master
+
+# nodejs
+#git submodule add https://github.com/lilses/db migration/libs/db
+git config -f .gitmodules submodule.db.branch master
+#
+## flutter
+#git submodule add address market/packages/address -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add api market/packages/api -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add auth_id market/packages/auth_id -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add cart market/packages/cart -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add chat market/packages/chat -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add encrypted_auth_id market/packages/encrypted_auth_id -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add fcm market/packages/fcm -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add notification market/packages/notification -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add product market/packages/product -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add transferwise market/packages/transferwise -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add wallet market/packages/wallet -b master &&
+#git config -f .gitmodules submodule.
+#git submodule add gen_api market/packages/gen_api -b master
+#git config -f .gitmodules submodule.
---
+++ xremote.sh
@@ -0,0 +1,34 @@
+# rust
+git remote add -f lib_stripe https://github.com/lilses/lib_stripe &&
+git remote add -f macros_make_app https://github.com/lilses/macros_make_app &&
+git remote add -f lib_transferwise https://github.com/lilses/lib_transferwise &&
+git remote add -f my_state https://github.com/lilses/my_state &&
+git remote add -f macros_make_error https://github.com/lilses/macros_make_error &&
+git remote add -f macros_make_scope https://github.com/lilses/macros_make_scope &&
+git remote add -f macros_make_model https://github.com/lilses/macros_make_model &&
+git remote add -f lib_wallet https://github.com/lilses/lib_wallet &&
+git remote add -f lib_notification https://github.com/lilses/lib_notification &&
+git remote add -f lib_fcm https://github.com/lilses/lib_fcm &&
+git remote add -f lib_chat https://github.com/lilses/lib_chat &&
+git remote add -f lib_auth_id https://github.com/lilses/lib_auth_id &&
+git remote add -f macros_make_api_doc https://github.com/lilses/macros_make_api_doc &&
+git remote add -f lib_typesense https://github.com/lilses/lib_typesense &&
+git remote add -f lib_product https://github.com/lilses/lib_product &&
+
+# nodejs
+git remote add -f migration https://github.com/lilses/migration &&
+git remote add -f db https://github.com/lilses/db &&
+
+# flutter
+git remote add -f address https://github.com/lilses/address &&
+git remote add -f api https://github.com/lilses/api &&
+git remote add -f auth_id https://github.com/lilses/auth_id &&
+git remote add -f cart https://github.com/lilses/cart &&
+git remote add -f chat https://github.com/lilses/chat &&
+git remote add -f encrypted_auth_id https://github.com/lilses/encrypted_auth_id &&
+git remote add -f fcm https://github.com/lilses/fcm &&
+git remote add -f notification https://github.com/lilses/notification &&
+git remote add -f product https://github.com/lilses/product &&
+git remote add -f transferwise https://github.com/lilses/transferwise &&
+git remote add -f wallet https://github.com/lilses/wallet &&
+git remote add -f gen_api https://github.com/lilses/gen_api
